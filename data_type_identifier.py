c5afc4f782452ce8bf7ab5c26f8806e7c0bc097e
unknown <yannvok2@gmail.com>
2020-06-14 11:18:25 +0200

    new data identifier model

--- data_type_identifier.py
+++ data_type_identifier.py
@@ -1,21 +1,21 @@
 from tensorflow.keras import layers
+from sklearn.preprocessing import scale
+from numpy import sort, array
 import tensorflow as tf
 import pandas as pd
 import pickle
 
 class DataTypeIdentifier(object):
-    '''
-    This class is used to predict if a certain variable is either categorical or numerical.
-    '''
-    
+        
     def __init__(self, encoder=None):
-        '''
+        """
         The encoder is used to encode the target variable
-        The default value of the encoder is 'None' because we will not always need to encode the target variable. Especially if we only want to use the "predict method"
-        '''
+        The default value of the encoder is None because we will not always need to encode the target variable. Especially if we only want to use the "predict method"
+        """
         if encoder is not None:
             self.__encoder = encoder()
-
+            self.__encoder_2 = encoder()
+        
     def get_encoder(self):
         return self.__encoder
         
@@ -33,25 +33,49 @@ class DataTypeIdentifier(object):
                 pass     
         return data
     
-    def build_final_set(self, correctly_typed_data, target_variable=None):
+    def build_final_set(self, original_correctly_typed_data, target_variable=None):
         '''
         Function to build training/final set. 
-        We create our final features (is_float and unique_values) to help predict if a feature is either numerical or categorical. 
-        We also encode our target variable.
+        We create our final features (is_float and unique_values) to help predict if a feature is numerical or categorical 
+        We also encode our target variable
         '''
+        correctly_typed_data = original_correctly_typed_data.copy()
+        correctly_typed_data.dropna(inplace=True) 
         new_features_list = []
         for feature_name in correctly_typed_data:
             feature = correctly_typed_data[feature_name]
             #Checking feature data type: we create a variable "is_float" to check if the data type of the feature is float. The default value is "False" or 0
             is_float = 0 
             if feature.dtype == float:
-                is_float = 1        
+                is_float = 1       
+            
+            #Computing steps between one modality and the one following it: if steps are consstant and equal to 1, there's a good chance that the distribution is categorical
+            feature_encoded=feature
+            if feature.dtype == object:
+                feature_encoded=self.__encoder_2.fit_transform(feature)    
+            feature_encoded = sort(list(set(feature_encoded)))
+            
+            #Step for two succesive modalities
+            index=0
+            step=1     
+            while step == 1 and index<len(feature_encoded)-1:
+                step = feature_encoded[index+1] - feature_encoded[index]
+                index+=1
+            
+            #if every step is constant and equals to 1, we affect 1 to "one_step"
+            one_step = 0
+            if step==1:
+                one_step = 1   
+             
             #Counting unique values per feature
             unique_values = feature.nunique()
             #Summarizing everything in a list for every single feature
-            new_features_list.append([is_float, unique_values])
+            new_features_list.append([is_float, unique_values, one_step])
+        new_features_list       = array(new_features_list)
+        #We scale(mean:0 and std:1) the values in order to keep the big modalities within a certain range: we don't want "unique_values" modalities to be dominant but we also want to keep the outliers.
+        new_features_list[:,1]  = scale(new_features_list[:,1])
         #Dataframe depicting our new features
-        new_features = pd.DataFrame(new_features_list, columns=["is_float", "unique_values"])
+        new_features = pd.DataFrame(new_features_list, columns=["is_float", "unique_values", "one_step"])
         
         #Encoding our target variable
         target_variable_encoded = None
@@ -96,19 +120,18 @@ class DataTypeIdentifier(object):
         '''
         We finally get our predictions by doing some data preprocessing first(counting unique values and checking if a feature has the data type float).
         '''
-        # 1- We drop every empty cells because our features must not have any missing values if we want to detect their types without any problem
-        data.dropna(inplace=True)   
-        # 2- We keep the initial data types
+
+        # 1- We keep the initial data types
         accurately_typed_data = self.keep_initial_data_types(data)
-        # 3- We build our final_set for our model. No target variable is given because it is what we are trying to predict
-        final_set             = self.build_final_set(correctly_typed_data=accurately_typed_data)
-        # 4- We get our two features: "is_float" and "unique_values" 
+        # 2- We build our final_set for our model. !no target variable is given because it is what we are trying to predict
+        final_set             = self.build_final_set(original_correctly_typed_data=accurately_typed_data)
+        # 3- We get our two features: "is_float" and "unique_values" 
         features              = final_set["new_features"]
-        # 5- We get our predictions 
+        # 4- We get our predictions 
         predictions           = model.predict_classes(features)
-        # 6- We label our predictions. For instance 0 represents "categorical" and 1 represents "numerical"
+        # 5- We label our predictions. For instance 0 represents "categorical" and 1 represents "numerical"
         labeled_predictions   = self.label_predictions(predictions, mappings)
-        # 7- We finally summarize everything in a dataframe
+        # 6- We finally summarize everything in a dataframe
         final_predictions     = pd.DataFrame(labeled_predictions, columns=["Predictions"], index=data.columns)
         return final_predictions
 
