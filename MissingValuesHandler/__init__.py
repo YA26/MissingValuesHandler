6e0dc61128264dd7744b568a47095a6067229e49
unknown <yannvok2@gmail.com>
2020-06-15 11:01:39 +0200

    transform folder into package

---
+++ MissingValuesHandler/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from MissingValuesHandler.missing_data_handler import MissingDataHandler
+
--- __init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-from missing_data_handler.Missing_values_handler import MissingDataHandler
-
