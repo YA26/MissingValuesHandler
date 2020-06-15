a060ecac0e971bd05ca7be3375debba3f50015c9
unknown <yannvok2@gmail.com>
2020-06-15 12:00:59 +0200

    __init__.py updated

--- MissingValuesHandler/__init__.py
+++ MissingValuesHandler/__init__.py
@@ -1,2 +1,4 @@
 # -*- coding: utf-8 -*-
 from MissingValuesHandler.missing_data_handler import MissingDataHandler
+from MissingValuesHandler.custom_exceptions import  VariableNameError, TargetVariableNameError, NoMissingValuesError
+from MissingValuesHandler.data_type_identifier import DataTypeIdentifier
